31a5bee53f02fdd6abc85046cef3707d0dcd77c6
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-05-23 09:56:52 -0300

    Criado dependências Spring e adicionado classes básicas de configuração

---
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudanteEndPoint.java
@@ -0,0 +1,10 @@
+package br.com.devdojo.awesome.endpoint;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("studant")
+public class StudanteEndPoint {
+
+}
---
+++ src/main/java/br/com/devdojo/awesome/model/Studant.java
@@ -0,0 +1,12 @@
+package br.com.devdojo.awesome.model;
+
+public class Studant {
+    private String name;
+
+    public Studant(String name) {
+        this.name = name;
+    }
+
+    public Studant() {
+    }
+}
