6b9900a8c1b8335e905ce3c053fc9149aec7017d
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-05-25 21:48:08 -0300

    Adicionado dependencias para test

--- src/main/java/br/com/devdojo/awesome/SpringBootEssentialsApplication.java
+++ src/main/java/br/com/devdojo/awesome/SpringBootEssentialsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //classe que inicia para uma aplicação spring boot.
 @SpringBootApplication
 //é a mesma coisa de usar as 3 anotações @Configuration, @EnableAutoConfiguration e @ComponentScan
-public class ApplicationStart {
+public class SpringBootEssentialsApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationStart.class, args);
+        SpringApplication.run(SpringBootEssentialsApplication.class, args);
     }
 }
---
+++ src/test/java/br/com/devdojo/SpringBootEssentialsApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.devdojo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootEssentialsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
